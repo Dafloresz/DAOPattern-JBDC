ff099220b9c9215b69290df6b59d4e82f71c9fd8
Thiago Piazentin <thiagopiazentin3@outlook.com>
2025-12-13 14:16:50 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,18 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
-        Department obj = new Department(2, "Eletronics");
 
-        System.out.println(obj);
+        Department obj = new Department(2, "Eletronics");
+        Seller seller = new Seller(27, "Ichigo", "ichigo@gmail.com", LocalDate.of(1997, 6, 21), 2300.0, obj);
+
+        System.out.println(seller);
     }
 }
