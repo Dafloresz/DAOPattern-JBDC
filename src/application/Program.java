99e85a259143cb1d8ed6dd62418faefce81ae383
Thiago Piazentin <thiagopiazentin3@outlook.com>
2025-12-18 16:34:27 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,6 +6,8 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 
 
@@ -31,6 +33,13 @@ public class Program {
         for (Seller seller1 : sellerList) {
             System.out.println(seller1);
         }
+
+        System.out.println("\n Test 04 Seller INSERT ===");
+        Seller newSeller = new Seller(null, "Ichigo", "ichiguinho@gmail.com", LocalDate.of(1900, 12, 27), 7800.0, department);
+        sellerDao.insert(newSeller);
+
+        System.out.println("Inserted! New id = " + newSeller.getId());
+
 
 
     }
